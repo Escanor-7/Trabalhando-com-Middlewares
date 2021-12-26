d25b7411e9b7f734b29c54567eba64ea4299b63b
Escanor-7 <carlos.escanor01@gmail.com>
2021-12-25 22:15:00 -0300

    Working with middlewares

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 
-const { v4: uuidv4, validate } = require('uuid');
+const { v4: uuidv4, validate, v4 } = require('uuid');
 
 const app = express();
 app.use(express.json());
@@ -10,19 +10,58 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
-}
+  const { username } = request.headers;
+
+  const user = users.find((user) => user.username === username);
+
+  if (!user) return response.status(404).json({ error: "User not found!" });
+
+  request.user = user;
+
+  return next();
+};
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+  const todoRegistration = user.todos.length;
+
+  if (user.pro || todoRegistration < 10) next();
+
+  return response.status(403).json({
+    error: "User already has 10 registered todos. Upgrade to pro!"
+  });
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  const user = users.find((user) => user.username === username);
+
+  if (!user) return response.status(404).json({ error: "User not found!" });
+
+  if (!validate(id)) return response.status(400).json({ error: "Id is not valid!" });
+  
+  const todo = user.todos.find((todo) => todo.id === id);
+  
+  if (!todo) return response.status(404).json({ error: "Todo not found!" });
+  
+  request.user = user;
+  request.todo = todo;
+
+  return next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find((user) => user.id === id);
+
+  if(!user) return response.status(404).json({error: "User not found!"});
+  
+  request.user = user;
+  
+  return next();
 }
 
 app.post('/users', (request, response) => {
